523cc7fd79ceec1ade326ac78b1c38bbcbfc8a49
Andrey <tynyanyi@mail.ru>
2020-04-11 22:05:03 +0300

    add dots nav slider

--- js/slider.js
+++ js/slider.js
@@ -6,55 +6,112 @@ window.addEventListener("DOMContentLoaded", function(){
      */
 
     let slider = document.getElementById('slider'),
-        indexShow = 1,
-        sec = 5;
+        indexShow = 1;
     
     if(slider){
         let slids  = slider.querySelectorAll('.slider-item'),
-            num_slids = slids.length;
-        console.log(slids);
+            dotWraper   = slider.querySelector('.dots'),
+            dots   = dotWraper.querySelectorAll('.dot'),
+            num_slids = slids.length,
+            sec = 5;
         /* Скрываем все слайды */
-        show(slids, indexShow);
+        show(indexShow);
 
         /**
          * Прокрутка слайдера с задержкой sec *1000
          */
         setInterval(()=>{
-            show(slids,++indexShow);
+            show(++indexShow);
         }, sec *1000); // устанавливаем интервал перелистывания в милисекундах
-    }
 
-    function hideSlids(slids){
-        slids.forEach(slid => {
-            slid.classList.remove("show");
+
+        // /*кнопка назад , индекс -1*/
+        // prev.addEventListener('click', ()=>{
+        //     moveSlide(-1);
+        // });
+
+        // /* кнопка вперед, индекс + 1 */
+        // next.addEventListener('click', ()=>{
+        //     moveSlide(1);
+        // });
+
+        /* Нажатие на точки */
+        dotWraper.addEventListener('click', (event)=>{
+            for(let i=0; i<dots.length + 1; i++){
+                console.log(event);
+                if(event.target.classList.contains('dot') && event.target == dots[i-1]){
+                    currentSlider(i);
+                }
+            }
         });
-    }
 
-    function showSlid(slid){
-        console.log(slid);
-        slid.classList.add("show");
-    }
-
-    function show(slids, index){
-        
         /**
-         * Проверка на первый и последний слайдер
+        * перелистывание , увеличиваем индекс на единицу
+        */
+        function moveSlide(n){
+            showSlid(indexShow += n);
+        }
+            
+        /**
+         * отображение указанного слайда
          */
-        if(indexShow > slids.length){
-            indexShow = 1;
-            index = indexShow;
-        }
-        if(indexShow < 1){
-            indexShow = slids.length;
-            index = indexShow;
+        function currentSlider(n){
+            show(indexShow = n);
+            sec = 5;
         }
 
-        console.log(slids[index-1]);
+        function show(index){
+        
+            /**
+             * Проверка на первый и последний слайдер
+             */
+            if(indexShow > slids.length){
+                indexShow = 1;
+                index = indexShow;
+            }
+            if(indexShow < 1){
+                indexShow = slids.length;
+                index = indexShow;
+            }
+    
+            /* Скрываем все слайды */
+            hideSlids(slids);
+            /* Отключаем активную точку */
+            hideActionDot(dots);
+    
+            /* Показываем конкретный слайд */
+            showSlid(index - 1);
+            /* Ставим активную точку для него */
+            showDotAction(index - 1);
+        }
 
-        /* Скрываем все слайды */
-        hideSlids(slids);
 
-        /* Показываем конкретный слайд*/
-        showSlid(slids[index - 1]);
+        /**
+         * Скрыть все ьлоки слайдеров
+         */
+        function hideSlids(slids){
+            slids.forEach(slid => {
+                slid.classList.remove("show");
+            });
+        }
+    
+        /**
+         * Отключить выделение у активной точки
+         * @param {*} dots 
+         */
+        function hideActionDot(dots){
+            dots.forEach(dot => {
+                dot.classList.remove("action");
+            });
+        }
+
+        function showSlid(i){
+            slids[i].classList.add("show");
+        }
+
+        function showDotAction(i){
+            dots[i].classList.add("action");
+        }
     }
+
 });
